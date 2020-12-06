14f8130dc68318e9148430386060fac6045511cc
Austin Schrader <austin.jschrader@gmail.com>
2020-12-06 14:21:05 -0800

    Connect to the database
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

---
+++ server/dbService.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log(error.message);
+  }
+  console.log('db ' + connection.state);
+})
