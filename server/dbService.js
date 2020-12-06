f1cf3ff68a55cec56d289bee3fd79a0dbfbca6eb
Austin Schrader <austin.jschrader@gmail.com>
2020-12-06 15:36:33 -0800

    Add a query to grab all data in names table
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- server/dbService.js
+++ server/dbService.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv');
+let instance = null;
 dotenv.config();
 
 const connection = mysql.createConnection({
@@ -14,5 +15,33 @@ connection.connect((error) => {
   if (error) {
     console.log(error.message);
   }
-  console.log('db ' + connection.state);
+  // console.log('db ' + connection.state);
 })
+
+class DbService {
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  async getAllData() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM names";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        })
+      });
+
+      // console.log(response);
+      return response;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+
+module.exports = DbService;
