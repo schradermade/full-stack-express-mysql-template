0c343d0f538c2df86c1cc58b5b0867a83758a051
Austin Schrader <austin.jschrader@gmail.com>
2020-12-06 18:13:48 -0800

    Add search functionality
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- server/dbService.js
+++ server/dbService.js
@@ -104,6 +104,25 @@ class DbService {
     }
   }
 
+  async searchByName(name)  {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM names WHERE name = ?";
+
+        connection.query(query, [name], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        })
+      });
+
+      // console.log(response);
+      return response;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 }
 
 
